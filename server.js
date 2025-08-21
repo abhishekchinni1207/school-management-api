e586890f71b9d53e7abf66adee42562db4197c8d
abhishekchinni1207 <chabhishek1207@gmail.com>
2025-08-21 07:32:10 +0530

    server.js

--- server.js
+++ server.js
@@ -3,16 +3,13 @@ const dotenv = require("dotenv");
 const schoolRoutes = require("./routes/schoolRoutes");
 
 dotenv.config();
-
 const app = express();
 
-// Middleware
+// Middleware to parse JSON body
 app.use(express.json());
 
-// Routes
-app.use("/", schoolRoutes);
+// Mount routes
+app.use("/api/schools", schoolRoutes);
 
 const PORT = process.env.PORT || 5000;
-app.listen(PORT, () => {
-  console.log(`🚀 Server running on port ${PORT}`);
-});
+app.listen(PORT, () => console.log(`🚀 Server running on port ${PORT}`));
